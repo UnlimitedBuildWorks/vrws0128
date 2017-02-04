eb5f0ccf1926ec06dc1e534f17f30e29354a86b1
里見 充則 <satomi@satomi-no-MacBook.local>
2017-02-04 15:49:54 +0900

    Complete code for Level 2.

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,15 +7,20 @@ public class CameraControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		#if UNITY_EDITOR
+//		Debug.Log(Screen.width);
+//		Debug.Log(Screen.height);
 		#endif
 		#if UNITY_IOS || UNITY_ANDROID
-		Input.gyro.enabled = true;
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			Input.gyro.enabled = true;
+		}
 		#endif
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		#if UNITY_EDITOR
+//		Debug.Log ("Updated");
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Rotate (Vector3.up * -Time.deltaTime * 100, Space.World);
 		}
@@ -30,7 +35,23 @@ public class CameraControl : MonoBehaviour {
 		}
 		#endif
 		#if UNITY_IOS || UNITY_ANDROID
-		transform.rotation = Quaternion.AngleAxis(90.0f, Vector3.right) * Input.gyro.attitude * Quaternion.AngleAxis(180.0f, Vector3.forward);
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			transform.rotation = Quaternion.AngleAxis(90.0f, Vector3.right) * Input.gyro.attitude * Quaternion.AngleAxis(180.0f, Vector3.forward);
+		}
 		#endif
+
+		// Level 2
+		//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Ray ray = new Ray();
+		ray.direction = Camera.main.transform.TransformDirection(Vector3.forward);
+		ray.origin = Vector3.zero;
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100)) {
+			//Debug.Log(Input.mousePosition);
+			Debug.Log (hit.transform.name);
+			Vector3 pos = hit.transform.position;
+			pos.y -= 0.001f;
+			hit.transform.position = pos;
+		}
 	}
 }
